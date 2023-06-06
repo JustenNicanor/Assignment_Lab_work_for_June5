49ebad4ecd8647d36b2ade55426c992d1eb6730d
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2023-06-06 23:27:56 +0800

    Add files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,25 @@
+public class PrimeNumbers {
+
+    public class Main {
+        public static void main(String[] args) {
+            int num = 29; // this is the number to test
+            boolean isPrime = true;
+
+            for(int i = 2; i < num; i++){
+                if(num % i==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime) {
+                System.out.println(num + " is a prime number.");
+            } else {
+                System.out.println(num + " is not a prime number.");
+            }
+        }
+    }
+    
+}
+
+
